abdd8cd35a293bf2461e7178c7a61a726a5be1e9
JoeB-G <joeboddenglennon@hotmail.co.uk>
2023-03-30 14:47:46 +0100

    add routers and sub routers to breakdown app logic

--- app.js
+++ app.js
@@ -1,41 +1,15 @@
 const express = require("express");
 const {
-  getCategories,
-  getReview,
-  getReviews,
-  getComments,
-  postComment,
-  patchReview,
-  deleteComment,
-  getUsers,
-  getApi,
-} = require("./controllers");
-const {
   handleCustomErrors,
   handleRouteErrors,
   handlePsqlErrors,
 } = require("./errorMiddleware");
 const app = express();
+const apiRouter = require("./Routes/api-router");
 
 app.use(express.json());
 
-app.get(`/api/categories`, getCategories);
-
-app.get(`/api/reviews/:review_id`, getReview);
-
-app.get("/api/reviews", getReviews);
-
-app.get("/api/reviews/:review_id/comments", getComments);
-
-app.post("/api/reviews/:review_id/comments", postComment);
-
-app.patch("/api/reviews/:review_id", patchReview);
-
-app.delete("/api/comments/:comment_id", deleteComment);
-
-app.get("/api/users", getUsers);
-
-app.get("/api", getApi);
+app.use("/api", apiRouter);
 
 app.use(handlePsqlErrors);
 
